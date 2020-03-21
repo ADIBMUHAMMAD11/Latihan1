d724a0fedea4dd068242ad9eefce7fc075d6dfcf
ammar alamudi <muhammadadiblutfi11@gmail.com>
2020-03-21 15:29:26 +0700

    perintah7

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
+$x = 10;
+$x += 2;
+	echo $x "</br>";
 
-$nama_depan = "Adib";
-$nama_belakang = "Muhammad";
+$y = 15;
+$y -= 5;
+	echo $y "</br>"
 
-	echo $nama_depan . "" . $nama_belakang;
-	
+$y = 10;
+$y /= 2;
+	echo $y "</br>"
+
+$y = 20;
+$y %= 4;
+	echo $y "</br>"
+
+$y = 30;
+$y .= 10;
+	echo $y "</br>"				
 ?>
