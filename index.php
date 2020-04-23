2422c92288de65f7a10ec32557cd797bbd10bdfc
ammar alamudi <muhammadadiblutfi11@gmail.com>
2020-04-23 20:07:05 +0700

    answer2

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <?php
 <?php
-$angka=8;
+$nama="muhammad adib lutfi";
+$umur =19;
 
-echo "nilai angka= $angka";
+echo "nama saya adalah $nama";
 echo "<br>";
-echo $angka+2;
-echo "<br>";
-echo $angka-2;
-echo "<br>";
-echo $angka+22;
-echo "<br>";
-echo $angka+2;
-echo "<br>";
-echo $angka+3;
-echo "<br>";
-echo $angka+2;
-
-
+echo "umur saya adalah $umur tahun";
 
 ?>
